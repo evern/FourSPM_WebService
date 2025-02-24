6b8967acbab816c479efec4bb2514dee50aa06dc
evern <evern@outlook.com>
2025-02-24 13:42:08 +0800

    Clean up logging and EdmModel configuration

--- Data/Extensions/EdmModelBuilder.cs
+++ Data/Extensions/EdmModelBuilder.cs
@@ -12,27 +12,10 @@ namespace FourSPM_WebService.Data.Extensions
 
             builder.EnableLowerCamelCase();
 
-            var projectsType = builder.EntitySet<ProjectEntity>("Projects").EntityType;
-            projectsType.HasKey(p => p.Guid); // Ensure OData has a primary key
-
-            // Define properties (not needed unless customization required)
-            projectsType.Property(p => p.ClientNumber);
-            projectsType.Property(p => p.ProjectNumber);
-            projectsType.Property(p => p.Name);
-            projectsType.Property(p => p.ClientContact);
-            projectsType.Property(p => p.PurchaseOrderNumber);
-
-            // Define the entity set AFTER registering the type
-            builder.EntitySet<ProjectEntity>("Projects");
-
+            builder.EntitySet<ProjectEntity>("Projects").EntityType.HasKey(p => p.Guid);
             builder.EntitySet<UserEntity>("Users");
-
-            // Explicitly set namespace and container
-            builder.Namespace = "FourSPM_WebService.Data.OData.FourSPM";
-            builder.ContainerName = "FourSPMContainer";
 
             return builder.GetEdmModel();
         }
     }
-
 }
